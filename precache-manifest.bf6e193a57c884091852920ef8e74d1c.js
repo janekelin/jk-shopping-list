3325e2a117e6584824c84bf905a170eeb6dc168f
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-08-12 13:12:14 +0200

    Updates

---
+++ precache-manifest.bf6e193a57c884091852920ef8e74d1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1c626a6c1eedae4519f0fac8e44b30",
+    "url": "/jk-shopping-list/index.html"
+  },
+  {
+    "revision": "78e7202813745120fafe",
+    "url": "/jk-shopping-list/static/css/main.af772b5c.chunk.css"
+  },
+  {
+    "revision": "8711407393d4d87012f4",
+    "url": "/jk-shopping-list/static/js/2.aa6d897e.chunk.js"
+  },
+  {
+    "revision": "78e7202813745120fafe",
+    "url": "/jk-shopping-list/static/js/main.7c2e0ce3.chunk.js"
+  },
+  {
+    "revision": "9a634e212d9792c82908",
+    "url": "/jk-shopping-list/static/js/runtime~main.be329988.js"
+  }
+]);
